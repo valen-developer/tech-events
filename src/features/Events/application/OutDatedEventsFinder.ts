d514028d777f6b2a7cc6ac84efb4506ad7c928d8
Valentín Valverde <valenreta.developer@gmail.com>
2022-10-18 15:40:29 +0200

    feat: outdated events implementation

---
+++ src/features/Events/application/OutDatedEventsFinder.ts
@@ -0,0 +1,20 @@
+import { Paginated } from "../../../core/types/Paginated.type";
+import { PageNumber } from "../../Shared/domain/valueObjects/PageNumber.valueObject";
+import { TechEventRepository } from "../domain/interfaces/TechEventRepository.interface";
+import { TechEvent } from "../domain/TechEvent.model";
+
+export class OutDatedEventsFinder {
+  constructor(private eventRepository: TechEventRepository) {}
+
+  public async findOutDatedEvents(props: {
+    page: number;
+  }): Promise<Paginated<TechEvent[], "events">> {
+    const page = new PageNumber(props.page);
+
+    const { events, pages } = await this.eventRepository.findOutDatedEvents(
+      page.valueOf()
+    );
+
+    return { events, pages };
+  }
+}
--- src/features/Events/domain/interfaces/TechEventRepository.interface.ts
+++ src/features/Events/domain/interfaces/TechEventRepository.interface.ts
@@ -1,5 +1,11 @@
+import { Paginated } from "../../../../core/types/Paginated.type";
 import { TechEvent } from "../TechEvent.model";
 
 export abstract class TechEventRepository {
-  public abstract findNextEvents(page: number): Promise<TechEvent[]>;
+  public abstract findNextEvents(
+    page: number
+  ): Promise<Paginated<TechEvent[], "events">>;
+  public abstract findOutDatedEvents(
+    page: number
+  ): Promise<Paginated<TechEvent[], "events">>;
 }
