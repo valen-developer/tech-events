e84740b0b0b8a788af24d4b1ae9156310acc28df
Valentín Valverde <valenreta.developer@gmail.com>
2022-10-19 13:14:52 +0200

    feat: styling

--- src/features/Events/presentation/components/TechEventCard/TechEventCard.tsx
+++ src/features/Events/presentation/components/TechEventCard/TechEventCard.tsx
@@ -1,13 +1,35 @@
-import { FC } from "react";
+import Image from "next/image";
+import { FC, useEffect, useState } from "react";
 import { TechEvent } from "../../../domain/TechEvent.model";
+
+import styles from "./TechEventCard.module.scss";
 
 interface TechEventCardProps {
   event: TechEvent;
 }
 
 export const TechEventCard: FC<TechEventCardProps> = ({ event }) => {
+  const [imgUrl, setImgUrl] = useState("https://picsum.photos/200/300");
+  const url = "https://picsum.photos/200/300";
+
+  const imageLoader = () => {
+    return url;
+  };
+
+  useEffect(() => {
+    setImgUrl(url);
+  }, [event]);
+
   return (
-    <div>
+    <div className={styles.card}>
+      <Image
+        loader={imageLoader}
+        src={imgUrl}
+        alt={`${event.title.value} cover`}
+        layout="responsive"
+        width="100"
+        height="100"
+      />
       <span>{event.title.value}</span>
       <span>{event.shortDescription.value}</span>
       <span>{event.getInitDate().toDDMMYYYY()}</span>
--- src/features/Events/presentation/components/TechEventCollection/TechEventCollection.tsx
+++ src/features/Events/presentation/components/TechEventCollection/TechEventCollection.tsx
@@ -2,6 +2,8 @@ import { FC } from "react";
 import { TechEvent } from "../../../domain/TechEvent.model";
 import { TechEventCard } from "../TechEventCard/TechEventCard";
 
+import styles from "./TechEventCollection.module.scss";
+
 interface TechEventCollectionProps {
   events: TechEvent[];
 }
@@ -10,7 +12,7 @@ export const TechEventCollection: FC<TechEventCollectionProps> = ({
   events,
 }) => {
   return (
-    <div>
+    <div className={styles.collection}>
       {events.map((e) => (
         <TechEventCard key={e.uuid.value} event={e} />
       ))}
