1d1f9d64a489004c8c56ebeaa08da8c43bc47320
Valentín Valverde <valenreta.developer@gmail.com>
2022-10-21 14:28:21 +0200

    feat: styling home header

--- src/features/Shared/presentation/components/HomeHeader/HomeHeader.tsx
+++ src/features/Shared/presentation/components/HomeHeader/HomeHeader.tsx
@@ -4,11 +4,20 @@ export const HomeHeader = () => {
   return (
     <header className={styles.header}>
       <section className={styles.header__message}>
-        <h1>Todos los eventos tecnológicos en un mismo lugar.</h1>
+        <h1>
+          <span className={styles.gradient}>
+            Todos los eventos tecnológicos
+          </span>{" "}
+          <br />
+          en un mismo lugar.
+        </h1>
       </section>
 
       <section className={styles.header__image}>
-        <img src="/images/hero_image.svg" alt="a girl with notebook sitting" />
+        <img
+          src="/images/hero_image.svg"
+          alt="A illustration where we see a girl sitting with her notebook. Also, we see a lamp illustrating a good idea"
+        />
       </section>
     </header>
   );
