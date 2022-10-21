53af5abdd0584fb1c7f967060ea56824836b4bf7
Valentín Valverde <valenreta.developer@gmail.com>
2022-10-21 12:21:30 +0200

    feat: outdated events are on screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,13 @@
 import type { NextPage } from "next";
 import { NextEventsCollection } from "../features/Events/presentation/components/NextEventsCollection/NextEventsCollection";
+import { OutdatedEventsCollection } from "../features/Events/presentation/components/OutdatedEventsCollection/OutdatedEventsCollection";
 import { AppLayout } from "../features/Shared/presentation/layout/AppLayout/AppLayout";
 
 const Home: NextPage = () => {
   return (
     <AppLayout>
       <NextEventsCollection />
+      <OutdatedEventsCollection/>
     </AppLayout>
   );
 };
