c35eb79fb19b4207a77868644ee39289c1c5092a
Valentín Valverde <valenreta.developer@gmail.com>
2022-10-21 14:39:56 +0200

    feat: styling app layout

--- src/features/Shared/presentation/layout/AppLayout/AppLayout.tsx
+++ src/features/Shared/presentation/layout/AppLayout/AppLayout.tsx
@@ -1,9 +1,16 @@
 import { FC } from "react";
 
+import styles from "./AppLayout.module.scss";
+
 interface AppLayoutProps {
   children: React.ReactNode;
 }
 
 export const AppLayout: FC<AppLayoutProps> = ({ children }) => {
-  return <div>{children}</div>;
+  return (
+    <div>
+      <div className={styles.navbar__container}></div>
+      <div className={styles.content__container}>{children}</div>
+    </div>
+  );
 };
